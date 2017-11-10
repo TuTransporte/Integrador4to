ae981a0a73d09c1789f114fe2dbfcbfcbfb968db
Bryan Alonso Basulto Avilez <pilotoaeronautico@hotmail.com>
2017-11-09 22:00:11 -0600

    Login y FrontEnd

---
+++ ProyectoTuTransporte/Controllers/FrontEndController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTuTransporte.Controllers
+{
+    public class FrontEndController : Controller
+    {
+        // GET: FrontEnd        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Para poder visualizar la vista Login | Bryan
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}

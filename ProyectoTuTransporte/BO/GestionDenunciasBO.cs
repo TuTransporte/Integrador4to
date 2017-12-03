95c9bacb9054adc6aef38bb7b91259cc4755ee1e
y2jmo <y2jmo@LAPTOP-VMV67UNM>
2017-12-03 17:30:13 -0600

    Clases para Denuncias creadas, listas para usar. Archivos .xsd creados para reportes

---
+++ ProyectoTuTransporte/BO/GestionDenunciasBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTuTransporte.BO
+{
+    public class GestionDenunciasBO
+    {
+        public string Id { get; set; }
+        public string Denuncia { get; set; }
+        public string FechaHora { get; set; }
+        public int Estado { get; set; }
+        public int FK_Ubicacion { get; set; }
+        public int FK_Usuario { get; set; }
+        public int FK_Chofer { get; set; }
+        public int FK_Camion { get; set; }
+    }
+}
---
+++ ProyectoTuTransporte/DAO/GestionDenunciasDAO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using ProyectoTuTransporte.BO;
+
+namespace ProyectoTuTransporte.DAO
+{
+    public class GestionDenunciasDAO
+    {
+
+        SqlCommand cmd;
+        ConexionDAO conex = new ConexionDAO();
+
+
+
+        public DataTable ListDenunciasPend()
+        {
+            String cadena = string.Format("SELECT Id, Denuncia, FechaHora FROM Denuncias WHERE Estado = '0'");
+            return conex.EjercutarSentenciaBusqueda(cadena);
+        }
+        public DataTable ListDenunciasApro()
+        {
+            String cadena = string.Format("SELECT Denuncia, FechaHora FROM Denuncias WHERE Estado = '1'");
+            return conex.EjercutarSentenciaBusqueda(cadena);
+        }
+        public DataTable ListDenunciasRech()
+        {
+            String cadena = string.Format("SELECT Denuncia, FechaHora FROM Denuncias WHERE Estado = '2'");
+            return conex.EjercutarSentenciaBusqueda(cadena);
+        }
+        public DataTable LlenarCamposBtnDen(int IdEmpleados)
+        {
+            String cadena = "SELECT Denuncia, FechaHora, FK_Ubicacion, FK_Usuario, FK_Chofer, FK_Camion FROM Denuncias WHERE Id = '" + IdEmpleados + "';";
+            return conex.EjercutarSentenciaBusqueda(cadena);
+        }
+
+
+        public int AgregarDenuncia(GestionDenunciasBO oDenuncias)
+        {
+            cmd = new SqlCommand("INSERT INTO Denuncias (Denuncia, FechaHora, FK_Ubicacion, FK_Usuario, FK_Chofer, FK_Camion, Estado) VALUES ('" + oDenuncias.Denuncia + "', GETDATE(), '" + oDenuncias.FK_Ubicacion + "', '" + oDenuncias.FK_Usuario + "', '" + oDenuncias.FK_Chofer + "', '" + oDenuncias.FK_Camion + "', '0')");
+            return conex.EjecutarComando(cmd);
+        }
+        public int ModificarAprovado(GestionDenunciasBO oDenuncias)
+        {
+            cmd = new SqlCommand("UPDATE Denuncias SET Estado = 1 WHERE Id='" + oDenuncias.Id + "'");
+            return conex.EjecutarComando(cmd);
+        }
+        public int ModificarRechazado(GestionDenunciasBO oDenuncias)
+        {
+            cmd = new SqlCommand("UPDATE Denuncias SET Estado = 2 WHERE Id='" + oDenuncias.Id + "'");
+            return conex.EjecutarComando(cmd);
+        }
+
+
+    }
+}
